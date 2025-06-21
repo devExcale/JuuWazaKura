c39c08cbe5cde2d4978286507fe02e6a55574d6a
Excale <dev_excale@hotmail.com>
2025-06-21 18:46:20 +0200

    Fixed CNN3DRNN logic
    Signed-off-by: Excale <dev_excale@hotmail.com>

--- jwk/model/arch/cnn3d_rnn.py
+++ jwk/model/arch/cnn3d_rnn.py
@@ -1,6 +1,7 @@
 import tensorflow as tf
 from keras import Sequential, Model
-from keras.layers import Conv3D, MaxPooling3D, GlobalAveragePooling3D, RNN, GRUCell, Dense
+from keras.layers import Conv3D, MaxPooling3D, GlobalAveragePooling3D, RNN, GRUCell, Dense, GlobalAveragePooling2D, \
+	TimeDistributed
 
 
 class CNN3DRNN(Model):
@@ -19,16 +20,18 @@ class CNN3DRNN(Model):
 
 		self.__model__()
 
+		return
+
 	def __model__(self):
 		"""
 		Build the CNN3DRNN model.
 		"""
-		input_shape = (self.window_size, self.frame_size[0], self.frame_size[1], 3)
+		input_shape = (None, self.frame_size[0], self.frame_size[1], 3)
 
 		# 3D CNN layers
 		self.conv11 = Conv3D(32, (3, 3, 3), activation='relu', padding='same', input_shape=input_shape)
 		self.conv12 = Conv3D(32, (3, 3, 3), activation='relu', padding='same')
-		self.pool1 = MaxPooling3D((1, 2, 2))  # Pooling only in spatial dimensions
+		self.pool1 = MaxPooling3D((1, 2, 2))
 
 		self.conv21 = Conv3D(64, (3, 3, 3), activation='relu', padding='same')
 		self.conv22 = Conv3D(64, (3, 3, 3), activation='relu', padding='same')
@@ -36,7 +39,7 @@ class CNN3DRNN(Model):
 
 		self.conv31 = Conv3D(128, (3, 3, 3), activation='relu', padding='same')
 		self.conv32 = Conv3D(128, (3, 3, 3), activation='relu', padding='same')
-		self.pool3 = GlobalAveragePooling3D()
+		self.pool3 = MaxPooling3D((1, 2, 2))
 
 		self.cnn = Sequential([
 			self.conv11,
@@ -49,6 +52,9 @@ class CNN3DRNN(Model):
 			self.conv32,
 			self.pool3
 		])
+
+		# Apply 2d pooling for each timestep
+		self.td_gap = TimeDistributed(GlobalAveragePooling2D())
 
 		# RNN layers
 		self.rnn1 = RNN(GRUCell(64), return_sequences=True)
@@ -71,41 +77,24 @@ class CNN3DRNN(Model):
 		:param training: Whether the model is in training mode.
 		:return: Dictionary of outputs (throw_output, tori_output)
 		"""
-		# Create sliding windows of frames
-		timesteps = inputs.shape[1]  # Number of frames in the video
-		windowed_inputs = tf.image.extract_patches(
-			images=inputs,
-			sizes=[1, self.window_size, 1, 1, 1],
-			strides=[1, 1, 1, 1, 1],
-			rates=[1, 1, 1, 1, 1],
-			padding='VALID'
-		)
-		# Reshape to (batch_size, new_timesteps, window_size, H, W, C)
-		new_timesteps = timesteps - self.window_size + 1
-		windowed_inputs = tf.reshape(
-			windowed_inputs,
-			(-1, new_timesteps, self.window_size, self.frame_size[0], self.frame_size[1], 3)
-		)
 
-		# Apply 3D CNN to each block
-		x_permuted = tf.transpose(windowed_inputs, perm=[1, 0, 2, 3, 4, 5])  # (new_timesteps, batch_size, ...)
-		processed_blocks = tf.map_fn(
-			lambda block: self.cnn(block, training=training),
-			x_permuted
-		)
-		# Transpose back to (batch_size, new_timesteps, cnn_output_features)
-		x_rnn_input = tf.transpose(processed_blocks, perm=[1, 0, 2])
+		# Pass through 3D CNN layers
+		x = self.cnn(inputs, training=training)
+
+		# Reduce dim to (batch_size, timesteps, features)
+		x = self.td_gap(x, training=training)
 
 		# Pass through RNN and Dense layers
-		x = self.rnn1(x_rnn_input, training=training)
+		x = self.rnn1(x, training=training)
 		x = self.rnn2(x, training=training)
 		x = self.rnn3(x, training=training)
 
-		x = self.dense1(x)
-		x = self.dense2(x)
+		# Pass RNN features through MLP
+		x = self.dense1(x, training=training)
+		x = self.dense2(x, training=training)
 
-		throw_output = self.dense_throw(x)
-		tori_output = self.dense_tori(x)
+		throw_output = self.dense_throw(x, training=training)
+		tori_output = self.dense_tori(x, training=training)
 
 		return {
 			'throw': throw_output,
