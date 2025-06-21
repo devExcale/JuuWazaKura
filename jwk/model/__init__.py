862cc0b4a7378f03456984369fe8ce4a601ffb4d
Excale <dev_excale@hotmail.com>
2025-06-21 19:08:29 +0200

    Ignore TFA warning
    Signed-off-by: Excale <dev_excale@hotmail.com>

--- jwk/model/__init__.py
+++ jwk/model/__init__.py
@@ -1,3 +1,12 @@
+import warnings
+
+warnings.filterwarnings(
+	"ignore",
+	message=r"\s*TensorFlow Addons.*",
+	category=UserWarning,
+	module="tensorflow_addons.utils.tfa_eol_msg",
+)
+
 from .frame_box import FrameBox
 from .model import JwkModel
 from .tracker import Tracker
